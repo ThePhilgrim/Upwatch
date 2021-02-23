b91e0c3616eb3923ec2b13f7cd402384bec97fd3
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2021-02-23 14:10:30 +0100

    Fix bug in extract_hourly_price(), clear some comments/todos

--- upwatch.py
+++ upwatch.py
@@ -8,14 +8,36 @@ import time
 # TODO: Program needs to log into Upwork when clicking job url
 
 
+# Class will be used by the GUI to access logic variables.
+class UserInput:
+    def __init__(
+        self,
+        url,
+        fixed_low_rate=None,
+        hourly_low_rate=None,
+        username=None,
+        password=None,
+    ):
+        self.url = url
+        self.fixed_low_rate = fixed_low_rate
+        self.hourly_low_rate = hourly_low_rate
+        self.username = username
+        self.password = password
+
+
+#   !self.user_agent here ?????
+
+
 def read_from_json(fallback_job_posts):
     """ Reads all the job posts from job_posts.json """
-    try:  # Not sure if I need a try block
+    try:
         with open("job_posts.json", "r") as job_posts_json:
             json_content = json.load(job_posts_json)
             return json_content
     except FileNotFoundError:
-        print("File not found – Attempting to create one.")  # !Remove this when code is working properly.
+        print(
+            "File not found – Attempting to create one."
+        )  # !Remove this when code is working properly.
         json_content = {"Job Posts": fallback_job_posts}
         return json_content
         # TODO: Call function for running settings window
@@ -30,13 +52,14 @@ def write_to_json(job_post_list):
 
 def extract_hourly_price(hourly_payment_type):
     """ Returns the hourly payment as int for message_printer() if-statement """
-    if "–" in hourly_payment_type:
+    # TODO: Put in try block and return empty string if error?
+    if "-" in hourly_payment_type:
         # Accounts for job_post["Payment Type"] == "Hourly: $X.00–$Y.00"
         # Looking at the $Y to account for the payment range.
         return int(float(hourly_payment_type.split()[1].split("-")[1].lstrip("$")))
     else:
         # Accounts for job_post["Payment Type"] == "Hourly: $X.00"
-        return int(float(hourly_payment_type.split()[1].lstrip('$')))
+        return int(float(hourly_payment_type.split()[1].lstrip("$")))
 
 
 def extract_fixed_price(fixed_payment_type):
@@ -55,13 +78,17 @@ def message_printer(new_job_posts):
     selected_new_job_posts = []
 
     for job_post in new_job_posts:
+        # !print(job_post)  # Use this line to debug what job post might cause an error.
         # job_post["Payment Type"] can be "Fixed-price", "Hourly: $X.00–$Y.00", or "Hourly"
         if (
             job_post["Payment Type"] == "Fixed-price"
             and extract_fixed_price(job_post["Budget"]) >= fixed_lowest_rate
         ):
             selected_new_job_posts.append(job_post)
-        elif (job_post["Payment Type"].split()[0] == "Hourly:" and extract_hourly_price(job_post["Payment Type"]) >= hourly_lowest_rate) or job_post["Payment Type"] == "Hourly":
+        elif (
+            job_post["Payment Type"].split()[0] == "Hourly:"
+            and extract_hourly_price(job_post["Payment Type"]) >= hourly_lowest_rate
+        ) or job_post["Payment Type"] == "Hourly":
             selected_new_job_posts.append(job_post)
 
     if len(selected_new_job_posts) == 0:
@@ -114,7 +141,7 @@ def job_post_scraper():
     """ Scrapes Upwork for job posts and stores details in variables """
     # TODO: Set url to input to let people use other searches. (Write it to json)
     # TODO: Control that input is valid upwork search link. (Regex library)
-
+    # TODO: Tell the user if there is no URL specified when trying to do request
     url = "https://www.upwork.com/ab/jobs/search/?page=2&q=(translat%20OR%20proofread)%20AND%20swedish&sort=recency"
 
     connection_attempts = 1
@@ -128,7 +155,7 @@ def job_post_scraper():
                 },
                 timeout=3,
             )  # TODO: Figure out how to fetch User Agent on current system.
-            response.raise_for_status()  # TODO: Find out why this doesn't work ("AttributeError: 'str' object has no attribute 'raise_for_status'")
+            response.raise_for_status()
             break
         except requests.exceptions.HTTPError as errh:
             print("HTTP Error:", errh)
@@ -175,14 +202,9 @@ def job_post_scraper():
             "URL": "https://upwork.com" + job_post_url,
         }
 
-        # TODO: Add logic for a "don't bother me rate" – if payment type == fixed-price and budget <= X: DELETE
-        # if payment type == Hourly and hourly rate <= X: DELETE
-
         job_post_list.append(job_post_dict)
 
-    json_difference_checker(
-        read_from_json(job_post_list), job_post_list
-    )
+    json_difference_checker(read_from_json(job_post_list), job_post_list)
 
     if job_post_list:
         write_to_json(job_post_list)

c6ac25e9d787101284c716c9d824894008f850e6
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2021-02-23 21:55:36 +0100

    Add skeleton of working gui code (PyQt5)

---
+++ upwatch_gui.py
@@ -0,0 +1,72 @@
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+
+
+class UpwatchGui:
+    def __init__(self):
+        self.app = QtWidgets.QApplication([])
+        self.app.setQuitOnLastWindowClosed(False)
+
+        # Create the icon
+        icon = QtGui.QIcon("uwlogo.png")
+
+        # Create the tray
+        self.tray = QtWidgets.QSystemTrayIcon()
+        self.tray.setIcon(icon)
+        self.tray.setVisible(True)
+
+        self.actions = []
+
+        # Create the menu
+        self.menu = QtWidgets.QMenu()
+        url_action = QtWidgets.QAction("Set URL")
+        url_action.triggered.connect(self.set_url_window)
+        self.actions.append(url_action)
+
+        settings_action = QtWidgets.QAction("Settings")
+        settings_action.triggered.connect(self.settings_window)
+        self.actions.append(settings_action)
+
+        about_action = QtWidgets.QAction("About")
+        about_action.triggered.connect(self.about_window)
+        self.actions.append(about_action)
+
+        # Add a Quit option to the menu.
+        quit_action = QtWidgets.QAction("Quit")
+        quit_action.triggered.connect(self.app.quit)
+        self.actions.append(quit_action)
+
+        # Add buttons to menubar menu
+        for action in self.actions:
+            self.menu.addAction(action)
+
+        # Add the menu to the tray
+        self.tray.setContextMenu(self.menu)
+
+    def set_url(self):
+        print("URL SET!")
+        self.set_url_window.close()
+        # value = app.set_url_window.text()
+        return
+
+    def set_url_window(self):
+        self.set_url_window = QtWidgets.QLineEdit("Set URL")
+        self.set_url_window.setGeometry(750, 50, 150, 50)
+        self.set_url_window.returnPressed.connect(lambda: self.set_url())
+        self.set_url_window.show()
+
+    def settings_window(self):
+        self.settings_window = QtWidgets.QWidget()
+        self.settings_window.setWindowTitle("Settings")
+        # self.settings_window.setGeometry(X, Y, X2, Y2)
+        self.settings_window.show()
+
+    def about_window(self):
+        self.about_window = QtWidgets.QWidget()
+        self.about_window.setWindowTitle("About")
+        # self.about_window.setGeometry(X, Y, X2, Y2)
+        self.about_window.show()
+
+
+gui = UpwatchGui()
+gui.app.exec_()
